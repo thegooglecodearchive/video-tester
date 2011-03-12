bda7de4e8327b94481350cb36ecdb7e16d98ceba
i.ucar86 <i.ucar86@c0338f10-77d1-c76e-8a41-47170384e066>
2011-03-12 18:51:06 +0000

    Added QoS-measure-collecting support for VQ measures

--- include/measures/vq.py
+++ include/measures/vq.py
@@ -1,6 +1,7 @@
 # coding=UTF8
 
 from measures import Meter, Measure
+from qos import QoSmeter
 from include.config import vtLog
 import math
 
@@ -16,10 +17,14 @@ class VQmeter(Meter):
             self.measures.append(VPSNR(data))
 
 class VQmeasure(Measure):
-    def __init__(self, files):
+    def __init__(self, (videodata, packetdata)):
         Measure.__init__(self)
-        self.yuv = files['received'][1]
-        self.yuvref = files['original'][1]
+        self.packetdata = packetdata
+        self.yuv = videodata['received'][1]
+        self.yuvref = videodata['original'][1]
+    
+    def getQoSm(self, measure):
+        return QoSmeter(measure, self.packetdata).run()
 
 class PSNR(VQmeasure):
     def __init__(self, data, component='Y'):
@@ -30,6 +35,7 @@ class PSNR(VQmeasure):
         self.measure['type'] = 'plot'
     
     def calculate(self):
+        L = 255
         width = self.yuv.video[self.cmp][0].shape[0]
         height = self.yuv.video[self.cmp][0].shape[1]
         fin = min(self.yuv.frames, self.yuvref.frames)
@@ -39,7 +45,7 @@ class PSNR(VQmeasure):
             sum = (self.yuv.video[self.cmp][i].astype(int) - self.yuvref.video[self.cmp][i].astype(int))**2
             mse = sum.sum() / width / height
             if mse != 0:
-                y.append(20 * math.log(255 / math.sqrt(mse), 10))
+                y.append(20 * math.log(L / math.sqrt(mse), 10))
             else:
                 y.append(100)
         self.graph(x, y)
@@ -52,3 +58,14 @@ class UPSNR(PSNR):
 class VPSNR(PSNR):
     def __init__(self, data):
         PSNR.__init__(self, data, 'V')
+
+##class SSIM(VQmeasure):
+##    def __init__(self, data):
+##        VQmeasure.__init__(self, data)
+##        self.measure['name'] = 'SSIM'
+##        self.measure['units'] = ['frame', 'dB']
+##        self.measure['type'] = 'plot'
+##    
+##    def calculate(self):
+##        L = 255
+##        
