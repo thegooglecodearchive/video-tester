1a3f0051b329464f9ce4d847d16fd90c62afa43a
i.ucar86 <i.ucar86@c0338f10-77d1-c76e-8a41-47170384e066>
2011-04-06 18:57:26 +0000

    Some fixes

--- setup.py
+++ setup.py
@@ -5,16 +5,56 @@
 ## This program is published under a GPLv3 license
 
 from distutils.core import setup
+import os, fnmatch
+
+## Code borrowed from wxPython's setup and config files
+## Thanks to Robin Dunn for the suggestion.
+## I am not 100% sure what's going on, but it works!
+def opj(*args):
+    path = os.path.join(*args)
+    return os.path.normpath(path)
+
+def find_data_files(base, srcdir, *wildcards, **kw):
+    # get a list of all files under the srcdir matching wildcards,
+    # returned in a format to be used for install_data
+    def walk_helper(arg, dirname, files):
+        if '.svn' in dirname:
+            return
+        names = []
+        lst, wildcards = arg
+        for wc in wildcards:
+            wc_name = opj(dirname, wc)
+            for f in files:
+                filename = opj(dirname, f)
+                
+                if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
+                    names.append(filename)
+        if names:
+            lst.append( (dirname.replace('doc', base), names ) )
+
+    file_list = []
+    recursive = kw.get('recursive', True)
+    if recursive:
+        os.path.walk(srcdir, walk_helper, (file_list, wildcards))
+    else:
+        walk_helper((file_list, wildcards), srcdir,
+                    [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
+    return file_list
+
+version = "0.1"
+files = find_data_files('share/doc/VideoTester-' + version, 'doc/', '*.*')
 
 setup(name = "VideoTester",
-    version = "0.1",
+    version = version,
     description = "Video Quality Assessment Tool",
     author = "Iñaki Úcar",
     author_email = "i.ucar86@gmail.com",
     url = "http://video-tester.googlecode.com",
     packages = ['VideoTester', 'VideoTester.measures'],
     scripts = ["VT"],
+    data_files = files,
     #long_description = """Really long text here.""" 
     platforms = ['Any'],
-    license = "GPLv3"
+    license = "GPLv3",
+    requires = []
 ) 
